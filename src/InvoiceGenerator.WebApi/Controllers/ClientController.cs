7c5d6fff07662332381e35b1c9f36ef219f6a07a
Jamie <jamie@gaprogman.com>
2022-07-02 16:16:07 +0100

    Started adding docs to controller

--- src/InvoiceGenerator.WebApi/Controllers/ClientController.cs
+++ src/InvoiceGenerator.WebApi/Controllers/ClientController.cs
@@ -1,6 +1,8 @@
+using Microsoft.AspNetCore.Mvc;
 using InvoiceGenerator.BusinessLogic;
 using InvoiceGenerator.ViewModels;
-using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Net;
 
 namespace InvoiceGenerator.WebApi.Controllers;
 
@@ -10,31 +12,45 @@ public class ClientController : ControllerBase
 {
     private readonly IClientService _clientService;
     private readonly ILogger<ClientController> _logger;
+
     public ClientController(IClientService clientService, ILogger<ClientController> logger)
     {
         _clientService = clientService;
         _logger = logger;
     }
 
+    /// <summary>
+    /// Used to get an UNPAGED list of all clients in the system.
+    /// </summary>
+    /// <returns>
+    /// An <see cref="IEnumerable{T}" /> where T is a <see cref="ClientViewModel"/>
+    /// </returns>
+    [SwaggerResponse((int)HttpStatusCode.OK, "The list of all Clients")]
     [HttpGet("/", Name = "GetClientList")]
     public IEnumerable<ClientViewModel> Get()
     {
         return _clientService.GetClients().ToList();
     }
 
+    /// <summary>
+    /// Used to get an UNPAGED list of all client names (with their IDs) known to the system.
+    /// </summary>
+    /// <returns>
+    /// An <see cref="IEnumerable{T}" /> where T is a <see cref="ClientNameViewModel"/>
+    /// </returns>
     [HttpGet("/Names", Name="GetClientNames")]
+    [SwaggerResponse((int)HttpStatusCode.OK, "The list of all Client Name and Id pairs")]
     public IEnumerable<ClientNameViewModel> GetNames()
     {
         return _clientService.GetClientNames();
     }
 
-    [HttpPost("/")]
     public int NewClient(ClientViewModel viewModel)
     {
         return _clientService.AddClient(viewModel);
     }
 
-    [HttpPost("/Page")]
+    [HttpPost("/Page", Name="GetPageOfClients")]
     public PagedResponse<ClientViewModel> GetPage(int pageNumber, int pageSize = 10)
     {
        return _clientService.GetPage(pageNumber, pageSize);
