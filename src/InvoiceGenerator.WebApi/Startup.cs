f789a12a66a0e670d1c924bfb34045b2e3cffc8c
Jamie Taylor <jamie.taylor@rjj-software.co.uk>
2022-07-04 22:39:27 +0100

    Greatly simplified the stastup.cs

--- src/InvoiceGenerator.WebApi/Startup.cs
+++ src/InvoiceGenerator.WebApi/Startup.cs
@@ -1,68 +1,59 @@
 using System.Diagnostics.CodeAnalysis;
-using Boxed.AspNetCore;
 using InvoiceGenerator.WebApi.Extensions;
+using Microsoft.EntityFrameworkCore;
 
 namespace InvoiceGenerator.WebApi;
 
 [ExcludeFromCodeCoverage]
     public class Startup
     {
-        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-            Environment = env;
         }
 
         public IConfiguration Configuration { get; }
-        public IWebHostEnvironment Environment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public virtual void ConfigureServices(IServiceCollection services)
         {
             var connectionString = Configuration.GetConnectionString("invoiceConnectionString");
-            services
-                .AddCustomCors()
-                .AddControllers().Services
-                .AddTransientServices(connectionString)
-                .AddMappers()
-                .AddRazorPages();
-
-            if (!Environment.IsEnvironment("IntegrationTesting"))
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddCors(opt =>
             {
-                services.AddCustomSwagger();
-            }
+                opt.AddPolicy(name: "AllowAny", builder =>
+                {
+                    builder.WithOrigins().AllowAnyHeader().AllowAnyOrigin();
+                });
+            });
+
+            services.AddRazorPages();
+
+            services.AddTransientServices(connectionString).AddMappers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app
-                .UseForwardedHeaders()
-                // .UseResponseCaching()
-                // .UseResponseCompression()
-                // .UseIf(
-                //     env.IsDevelopment(),
-                //     app => app.UseDeveloperExceptionPage())
-                .UseDeveloperExceptionPage()
-                .UseRouting()
-                // .UseAuthentication()
-                // .UseAuthorization()
-                .UseCors("AllowAny")
-                .UseEndpoints(
-                    builder => {
-                        builder
-                            .MapControllers()
-                            .RequireCors("AllowAny");
-                        builder.MapRazorPages();
-                    })
-                .UseIf(
-                    !env.IsEnvironment("IntegrationTesting"),
-                    app => app.UseSwagger()
-                )
-                .UseIf(
-                    !env.IsEnvironment("IntegrationTesting"),
-                    app => app.UseSwaggerUI(c =>
-                        c.SwaggerEndpoint("/swagger/v1/swagger.json", "InvoiceGenerator.WebApi v1"))
-                );
+            if (!env.IsEnvironment("IntegrationTesting"))
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "InvoiceGenerator.WebApi v1"));
+            }
+
+            app.UseCors("AllowAny");
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints => 
+            {
+                endpoints.MapControllers();
+                endpoints.MapRazorPages();
+            });
         }
     }
