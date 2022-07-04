5209a3ff500a3562024c63dde8aeb10f3783d8b6
Jamie Taylor <60719058+jamie-taylor-rjj@users.noreply.github.com>
2022-07-04 18:18:14 +0100

    Greatly simplified program.cs

--- src/InvoiceGenerator.WebApi/Program.cs
+++ src/InvoiceGenerator.WebApi/Program.cs
@@ -1,130 +1,20 @@
-using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
-using System.Reflection;
-using Boxed.AspNetCore;
-using Serilog;
-using Serilog.Core;
-using Serilog.Events;
 
 namespace InvoiceGenerator.WebApi
 {
     [ExcludeFromCodeCoverage]
     public class Program
     {
-        public static Task<int> Main(string[] args) => LogAndRunAsync(CreateHostBuilder(args).Build());
-
-        public static async Task<int> LogAndRunAsync(IHost host)
+        public static void Main(string[] args)
         {
-            if (host is null)
-            {
-                throw new ArgumentNullException(nameof(host));
-            }
-
-            // Use the W3C Trace Context format to propagate distributed trace identifiers.
-            // See https://devblogs.microsoft.com/aspnet/improvements-in-net-core-3-0-for-troubleshooting-and-monitoring-distributed-apps/
-            Activity.DefaultIdFormat = ActivityIdFormat.W3C;
-            Activity.ForceDefaultIdFormat = true;
-
-            host.Services.GetRequiredService<IHostEnvironment>().ApplicationName =
-                Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyProductAttribute>()?.Product;
-
-            Log.Logger = CreateLogger(host);
-
-            try
-            {
-                Log.Information("Started application");
-                await host.RunAsync().ConfigureAwait(false);
-                Log.Information("Stopped application");
-                return 0;
-            }
-#pragma warning disable CA1031 // Do not catch general exception types
-            catch (Exception exception)
-#pragma warning restore CA1031 // Do not catch general exception types
-            {
-                Log.Fatal(exception, "Application terminated unexpectedly");
-                return 1;
-            }
-            finally
-            {
-                Log.CloseAndFlush();
-            }
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            new HostBuilder()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .ConfigureHostConfiguration(
-                    configurationBuilder => configurationBuilder
-                        .AddEnvironmentVariables(prefix: "DOTNET_")
-                        .AddIf(
-                            args is object,
-                            x => x.AddCommandLine(args)))
-                .ConfigureAppConfiguration((hostingContext, config) =>
-                    AddConfiguration(config, hostingContext.HostingEnvironment, args))
-                .UseSerilog()
-                .UseDefaultServiceProvider(
-                    (context, options) =>
-                    {
-                        var isDevelopment = context.HostingEnvironment.IsDevelopment();
-                        options.ValidateScopes = isDevelopment;
-                        options.ValidateOnBuild = isDevelopment;
-                    })
-                .ConfigureWebHost(ConfigureWebHostBuilder)
-                .UseConsoleLifetime();
-
-        private static void ConfigureWebHostBuilder(IWebHostBuilder webHostBuilder) =>
-            webHostBuilder
-                .UseKestrel((builderContext, options) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    options.AddServerHeader = false;
-                    options.Limits.MaxRequestBodySize = 52428800; // 50 MB for now
-                    options.Limits.MaxRequestHeadersTotalSize = 1048576;
-                })
-                .UseStartup<Startup>();
-
-        private static IConfigurationBuilder AddConfiguration(
-            IConfigurationBuilder configurationBuilder,
-            IHostEnvironment hostEnvironment,
-            string[] args) =>
-            configurationBuilder
-                // Add configuration from the appsettings.json file.
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false)
-                // Add configuration from an optional appsettings.development.json, appsettings.staging.json or
-                // appsettings.production.json file, depending on the environment. These settings override the ones in
-                // the appsettings.json file.
-                .AddJsonFile($"appsettings.{hostEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: false)
-                // Add configuration from files in the specified directory. The name of the file is the key and the
-                // contents the value.
-                .AddKeyPerFile(Path.Combine(Directory.GetCurrentDirectory(), "configuration"), optional: true)
-                // This reads the configuration keys from the secret store. This allows you to store connection strings
-                // and other sensitive settings, so you don't have to check them into your source control provider.
-                // Only use this in Development, it is not intended for Production use. See
-                // http://docs.asp.net/en/latest/security/app-secrets.html
-                .AddIf(
-                    hostEnvironment.IsDevelopment() && !string.IsNullOrEmpty(hostEnvironment.ApplicationName),
-                    x => x.AddUserSecrets(Assembly.GetExecutingAssembly(), optional: true))
-                // Add configuration specific to the Development, Staging or Production environments. This config can
-                // be stored on the machine being deployed to or if you are using Azure, in the cloud. These settings
-                // override the ones in all of the above config files. See
-                // http://docs.asp.net/en/latest/security/app-secrets.html
-                .AddEnvironmentVariables()
-                // Add command line options. These take the highest priority.
-                .AddIf(
-                    args is object,
-                    x => x.AddCommandLine(args));
-
-        private static Logger CreateLogger(IHost host)
-        {
-            var hostEnvironment = host.Services.GetRequiredService<IHostEnvironment>();
-            return new LoggerConfiguration()
-                .ReadFrom.Configuration(host.Services.GetRequiredService<IConfiguration>())
-                .Enrich.WithProperty("Application", hostEnvironment.ApplicationName)
-                .Enrich.WithProperty("Environment", hostEnvironment.EnvironmentName)
-                .WriteTo.Console(LogEventLevel.Information)
-                .WriteTo.Conditional(
-                    x => !hostEnvironment.IsProduction(),
-                    x => x.Console().WriteTo.Debug())
-                .CreateLogger();
-        }
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
